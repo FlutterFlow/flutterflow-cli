25354d8fa9050269a6ab528cb63946da478cb973
Anton Emelyanov <anton@flutterflow.io>
2023-01-04 13:16:04 -0500

    Create new dart app from template

---
+++ bin/flutterflow_cli.dart
@@ -0,0 +1,5 @@
+import 'package:flutterflow_cli/flutterflow_cli.dart' as flutterflow_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutterflow_cli.calculate()}!');
+}
---
+++ test/flutterflow_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutterflow_cli/flutterflow_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
